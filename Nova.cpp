24349152014047c47286920c60fcbd40a847e4e9
Selvius <pmedlin13@gmail.com>
2023-05-03 12:47:39 -0500

    Update Nova.cpp

--- Nova.cpp
+++ Nova.cpp
@@ -1,50 +1,67 @@
-// Include Dlib in your C++ program
 #include <dlib/dnn.h>
 
-// Define some constants for the model architecture and parameters
-const int num_classes = 10; // Number of classes to predict
-const int num_epochs = 10; // Number of epochs to train the model
-const int batch_size = 32; // Batch size for training the model
+// Define some constants for the model architecture and parameters 
+const int num_classes = 10; // Number of classes to predict 
+const int num_epochs = 10; // Number of epochs to train the model 
+const int batch_size = 32; // Batch size for training the model 
 const double learning_rate = 0.01; // Learning rate for training the model
 
-// Define the model using Dlib's tensors and modules
-using namespace dlib;
-using net_type = loss_multiclass_log<fc<num_classes,
-    relu<fc<100,
-    relu<fc<100,
-    input<matrix<unsigned char>>
-    >>>>>;
+// Define the model using Dlib’s tensors and modules using namespace dlib; 
+using net_type = loss_multiclass_log<fc<num_classes, relu<fc<100, relu<fc<100, input<matrix<unsigned char>> >>>>>;
 
-// Create an instance of the model on each node
+// Create an instance of the model on each node 
 net_type net;
 
-
-// Include FederatedAI in your C++ program
+// Include FederatedAI
 #include <federatedai/federated_learning.h>
 
-// Define some constants for the federated learning settings and parameters
-const int num_rounds = 10; // Number of rounds of federated learning
-const double fraction = 0.1; // Fraction of nodes to participate in each round
-const double threshold = 0.01; // Threshold for stopping the federated learning
+// Define some constants for the federated learning settings and parameters 
+const int num_rounds = 10; 
+// Number of rounds of federated learning 
+const double fraction = 0.1; 
+// Fraction of nodes to participate in each round 
+const double threshold = 0.01; 
+// Threshold for stopping the federated learning
 
-// Create an instance of the federated learning algorithm on each node
+// Create an instance of the federated learning algorithm on each node 
 federatedai::FederatedLearning fl;
 
-// Load the local data set on each node
-std::vector<matrix<unsigned char>> images;
-std::vector<unsigned long> labels;
-dlib::load_mnist_dataset("/path/to/data", images, labels);
+// Load the local data set on each node 
+std::vector<matrix<unsigned char>> images; std::vector<unsigned long> labels; dlib::load_mnist_dataset(“/path/to/data”, images, labels);
 
-// Perform decentralized learning on the blockchain network using federated learning algorithms
-for (int round = 0; round < num_rounds; round++)
+// Perform decentralized learning on the blockchain network using federated learning algorithms 
+for (int round = 0; round < num_rounds; round++) { // Train the local model on each node using the local data set for one epoch 
+dnn_trainer<net_type> trainer(net); trainer.set_learning_rate(learning_rate); trainer.set_min_batch_size(batch_size); trainer.train_one_step(images, labels);
+
+// Create a transaction that contains the local model updates encrypted with the private key and broadcast it to the network
+std::vector<double> local_updates = net.get_parameters();
+std::string encrypted_update = fl.encrypt_and_broadcast(local_updates);
+
+// Receive the transactions from other nodes and verify their signatures
+std::vector<std::string> encrypted_updates = fl.receive_and_verify();
+
+// Decrypt the model updates from other nodes
+std::vector<std::vector<double>> updates;
+for (auto& eu : encrypted_updates)
 {
-    // Train the local model on each node using the local data set for one epoch
-    dnn_trainer<net_type> trainer(net);
-    trainer.set_learning_rate(learning_rate);
-    trainer.set_min_batch_size(batch_size);
-    trainer.train_one_step(images, labels);
+    std::vector<double> update = fl.decrypt(eu);
+    updates.push_back(update);
+}
 
-    // Create a transaction that contains the local model updates encrypted with the private key and broadcast it to the network
-    std::vector<double> local_updates = net.get_parameters();
-    std::string encrypted_updates = fl.encrypt(local_updates, private_key);
-    bitcoin-cli -conf=/path/to/bitcoin.conf sendtoaddress <node address> 0 <encrypted updates>
+// Aggregate the model updates from other nodes
+std::vector<double> aggregated_update = fl.aggregate(updates);
+
+// Update the global model with the aggregated update
+fl.update_model(aggregated_update);
+
+// Check if the global model has converged
+bool converged = fl.check_convergence(threshold);
+if (converged)
+{
+    std::cout << "Federated learning has converged after " << round + 1 << " rounds." << std::endl;
+    break;
+}
+}
+
+// Save the global model on each node 
+serialize(“/path/to/model”) << net;
